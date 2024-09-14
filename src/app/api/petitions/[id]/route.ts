c94826196290d7fc75738ac1fcd86acd2768af0a
wils <92552628+vvils@users.noreply.github.com>
2024-09-14 00:59:31 -0400

    Implemented full petitions CRUD and single petition view page

--- src/app/api/petitions/[id]/route.ts
+++ src/app/api/petitions/[id]/route.ts
@@ -17,3 +17,9 @@ export async function GET(req: NextRequest, { params }: Params) {
   const petition = await Petition.findOne({ _id: id });
   return NextResponse.json({ petition }, { status: 200 });
 }
+export async function DELETE(req: NextRequest, { params }: Params) {
+  const { id } = params;
+  await connectDB();
+  await Petition.findByIdAndDelete(id);
+  return NextResponse.json({ message: "del" }, { status: 200 });
+}
