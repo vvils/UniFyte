28c0fa6788a5608406c0a0c9fbeac45d434e8000
Cody Lam <143743411+codlam26@users.noreply.github.com>
2024-09-15 23:33:09 -0400

    Finishing up the drag and drop and the creating petition Page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,47 +2,75 @@
 import { useState, useEffect } from "react";
 import Tiptap from "@/components/textEditor";
 import DragAndDrop from "@/components/dragAndDrop";
-import { POST } from "../api/petitions/route";
+import NextNProgress from 'nextjs-progressbar';
 
 interface University {
   name: string;
   state: string;
 }
 
-//Testing Purposes
-const universities: University[] = [
-  { name: "Harvard University", state: "Massachusetts" },
-  { name: "Stanford University", state: "California" },
-  { name: "University of Chicago", state: "Illinois" },
-  { name: "MIT", state: "Massachusetts" },
-  { name: "Princeton University", state: "New Jersey" },
-  { name: "Yale University", state: "Connecticut" },
-  { name: "University of California, Berkeley", state: "California" },
-];
-
 export default function PetitionPage() {
+  //Pagination
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [activeButton, setActiveButton] = useState(false);
-  
+  const [activePage, setActivePage] = useState<string>("")
+
+  //Handles user inputs
   const [text, setText] = useState<string>("");
   const [title, setTitle] = useState<string>("");
 
-  const [searchedUniversity, setSearchedUniversity] = useState<string>("")
-  const [filteredUniversity, setFilteredUniversity] = useState<University[]>(universities)
+  //Handles University searching
+  const [universities, setUniversities] = useState<University[]>([]);
+  const [searchedUniversity, setSearchedUniversity] = useState<string>("");
+  const [filteredUniversity, setFilteredUniversity] = useState<University[]>(universities);
+
+  useEffect(() => {
+    const fetchUniversities = async () => {
+      try {
+        const response = await fetch("/api/universities");
+        const data = await response.json();
+
+        const mappedUniversities = data.map((university: any) => ({
+          objectId: university.objectId,
+          name: university.name,
+          state: university.state.name,
+        }));
+
+        setUniversities(mappedUniversities);
+        setFilteredUniversity(mappedUniversities);
+      } catch (error) {
+        console.error("Error fetching universities:", error);
+      }
+    };
+
+    fetchUniversities();
+  }, []);
+
+  const handleSearch = (e: any) => {
+    const searchValue = e.target.value.toLowerCase();
+    setSearchedUniversity(searchValue);
+
+    console.log(searchValue);
+
+    const filtered = universities.filter((university) =>
+      university.name.toLowerCase().includes(searchValue)
+    );
+    setFilteredUniversity(filtered);
+  };
 
   const incrementCounter = () => {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
   const decrementCounter = () => {
-    if(currentQuestionIndex > 0){
+    if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
     }
   };
 
   const handleContentChange = (content: any) => {
-    setText(content)
-  }
+    setText(content);
+  };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -75,18 +103,6 @@ export default function PetitionPage() {
     }
   };
 
-  const handleSearch = (e:any) => {
-    const searchValue = e.target.value.toLowerCase();
-    setSearchedUniversity(searchValue);
-
-    const filtered = universities.filter(
-      (university) =>
-        university.name.toLowerCase().includes(searchValue) ||
-        university.state.toLowerCase().includes(searchValue)
-    );
-    setFilteredUniversity(filtered);
-  };
-
   return (
     <main className="min-h-screen justify-between p-4">
       <div className="flex justify-center mt-16">
@@ -97,7 +113,7 @@ export default function PetitionPage() {
                 What University do you go to?
               </h1>
               <h1 className="text-2xl mt-8">Connect with people near you</h1>
-              
+
               <input
                 type="text"
                 value={searchedUniversity}
@@ -105,39 +121,43 @@ export default function PetitionPage() {
                 placeholder="Enter a University"
                 className="mt-8 w-full p-4 rounded-2xl border-2 border-black"
               />
-                
+
               {searchedUniversity && (
-                  <div className="mt-4 border border-gray-200 rounded-md shadow-lg max-h-64 overflow-y-auto">
-                    {filteredUniversity.length > 0 ? (
-                      filteredUniversity.map((university, index) => (
-                        <div
-                          key={index}
-                          className="p-2 hover:bg-blue-100 cursor-pointer"
-                          onClick={() => setSearchedUniversity(university.name)}
-                        >
-                          <span className="font-semibold">{university.name}</span>, {university.state}
-                        </div>
-                      ))
-                    ) : (
-                      <p className="p-2">No universities found</p>
-                    )}
-                  </div>
-                )}
+                <div className="mt-4 border border-gray-200 rounded-md shadow-lg max-h-64 overflow-y-auto">
+                  {filteredUniversity.length > 0 ? (
+                    filteredUniversity.map((university, index) => (
+                      <div
+                        key={index}
+                        className="p-2 hover:bg-blue-100 cursor-pointer"
+                        onClick={() => setSearchedUniversity(university.name)}
+                      >
+                        <span className="font-semibold">{university.name}</span>
+                        , {university.state}
+                      </div>
+                    ))
+                  ) : (
+                    <p className="p-2">No universities found</p>
+                  )}
+                </div>
+              )}
             </div>
-      
           )}
 
           {currentQuestionIndex === 1 && (
-            <div className ="flex flex-col">
-              <h1 className="md:font-bold text-5xl">
-                How would you like to write your petition?
-              </h1>
-              <button className="text-2xl mt-12 pt-8 pb-8 border-2 rounded-xl">
-                Create Your Petition from Stratch
-              </button>
-              <button className="text-2xl mt-12 pt-8 pb-8 border-2 rounded-xl">
-                Generate Your Petition with AI This is a test
-              </button>
+            <div>
+              <div className="flex flex-col">
+                <h1 className="md:font-bold text-5xl">
+                  How would you like to write your petition?
+                </h1>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <button className="text-2xl mt-8 p-8 border-2 rounded-xl hover:border-orange-300">
+                  Create Your Petition from Stratch
+                </button>
+                <button className="text-2xl mt-8 p-8 border-2 rounded-xl hover:border-orange-300">
+                  Generate Your Petition with AI This is a test
+                </button>
+              </div>
             </div>
           )}
 
@@ -161,19 +181,18 @@ export default function PetitionPage() {
               <h1 className="md:font-bold text-5xl">
                 Write are you writing about?
               </h1>
-              <h1 className="items-center text-3xl mt-4">
-                Start from scratch
-              </h1>
+              <h1 className="items-center text-3xl mt-4">Start from scratch</h1>
               <Tiptap
-                onChange = {(text: string) => handleContentChange(text)}
-                content={text}/>
+                onChange={(text: string) => handleContentChange(text)}
+                content={text}
+              />
             </div>
           )}
 
           {currentQuestionIndex === 4 && (
             <div>
               <h1 className="md:font-bold text-5xl">Add an Image</h1>
-              <DragAndDrop/>
+              <DragAndDrop />
             </div>
           )}
 
@@ -182,35 +201,75 @@ export default function PetitionPage() {
               <h1 className="md:font-bold text-5xl">
                 Your petition is ready to make a difference
               </h1>
-              <button className="border-2 rounded-xl mt-12 text-xl p-4">Preview Petition</button>
-              <button className="border-2 rounded-xl mt-12 text-xl p-4" onClick={handleSubmit}>Create Petition</button>
+              <h1 className="text-2xl mt-6">Here's what's next:</h1>
+              <h1 className="text-2xl mt-6">Create your petition. You can edit your petition any time</h1>
+              <h1 className="text-2xl mt-6">Share it with the people you know</h1>
+              
+              <div className="flex justify-end mt-8">
+                <button className="border-black border-2 rounded-xl mt-12 text-xl p-4 hover:bg-gray"
+                onClick={() => {setActivePage("previewPage")}}>
+                  Preview Petition
+                </button>
+                
+                <button
+                  className="ml-6 border-black border-2 rounded-xl mt-12 text-xl p-4 bg-orange-300 text-white text-bold hover:border-green-300"
+                  onClick={() => {
+                    handleSubmit;
+                  }}
+                >
+                  Create Petition
+                </button>
+              </div>
+              
+            </div>
+          )}
+
+          {currentQuestionIndex === 6 && (
+            <div>
+              <button>Copy Link</button>
             </div>
           )}
 
           <div className="flex-row">
-              {currentQuestionIndex === 0 || currentQuestionIndex === 5 ? 
-              <div></div>: 
-              (
-                <button className="border-2 rounded-xl mt-12 text-xl p-3" onClick={() => {decrementCounter()}}>
-                  Back
-                </button>
-              )}
-              
-              {currentQuestionIndex === 5 ? 
-              <div></div> :
-              (
-                <button
-                  className="bg-blue-300 border-2 rounded-xl ml-4 mt-12 text-xl p-3"
-                  disabled={activeButton}
-                  onClick={() => {incrementCounter()}}
-                >
-                  Continue
-                </button>
-              )}
-              
+            {currentQuestionIndex === 0 || currentQuestionIndex === 5 ? (
+              <div></div>
+            ) : (
+              <button
+                className="border-2 rounded-xl mt-12 text-xl p-3"
+                onClick={() => {
+                  decrementCounter();
+                }}
+              >
+                Back
+              </button>
+            )}
+
+            {(currentQuestionIndex === 5  && activePage ==="") ? (
+              <></>
+            ) : (
+              <button
+                className="bg-blue-300 border-2 rounded-xl ml-4 mt-12 text-xl p-3"
+                disabled={activeButton}
+                onClick={() => {
+                  incrementCounter();
+                }}
+              >
+                Continue
+              </button>
+            )}
+
           </div>
+
         </div>
       </div>
     </main>
   );
+}
+
+function PreviewPage(){
+  return(
+    <div>
+      <DragAndDrop/>
+    </div>
+  )
 }
